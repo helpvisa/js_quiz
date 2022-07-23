991b8a80e1a2a200d50501f89f82b0f907b50623
dan brackenbury <daniel.brackenbury@gmail.com>
2022-07-23 19:50:52 -0400

    added random q picker, removes previously answered qs from q pool

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,41 @@
-// query the quiz container and store it
-var quizEl = document.querySelector("#quiz");
-// query the question header and store it (for updating text)
-var questionEl = document.querySelector("#qText");
-// query the question description and store it (also for updating text)
-var questionDescriptionEl = document.querySelector("#qDesc");
-// query the feedback result (right or wrong) for updating
-var questionResultEl = document.querySelector("#qRes");
+//******** global variable declarations ********//
+var quizEl = document.querySelector("#quiz"); // query the quiz container and store it
+var questionEl = document.querySelector("#qText"); // query the question header and store it (for updating text)
+var questionDescriptionEl = document.querySelector("#qDesc"); // query the question description and store it (also for updating text)
+var questionResultEl = document.querySelector("#qRes"); // query the feedback result (right or wrong) for updating
 
 // the question currently being displayed
 var currentQuestion = null;
+var currentQuestionIndex = 0;
 
-// create a test question object for testing purposes
-var question1 = {
-    question: "what is my favourite colour?",
-    a: ["green", "blue", "red", "burgundy"],
-    correct: 0,
-}
+// create questions object
+var questions = [
+    {
+        question: "Which brackets denote the beginning and end of an array?",
+        a: ["{}", "()", "[]", "<>"],
+        correct: 2,
+    },
+    {
+        question: "What is var f = function()?",
+        a: ["Function expression", "Function declaration", "Variable declaration", "Function equation"],
+        correct: 0,
+    },
+    {
+        question: "What can string values NOT be enclosed by?",
+        a: ["\"\"", "''", "**", "``"],
+        correct: 2,
+    },
+    {
+        question: "Which of the following is not a data type?",
+        a: ["boolean", "int", "float", "function"],
+        correct: 3,
+    },
+]
 
 
-initQuestion(question1);
+//******** main body of logic code ********//
+currentQuestionIndex = Math.floor(Math.random() * questions.length);
+initQuestion(questions[currentQuestionIndex]);
 
 // add listener to quiz container element to check its events for clicks
 quizEl.addEventListener("click", function(event) {
@@ -26,7 +43,6 @@ quizEl.addEventListener("click", function(event) {
     if (clicked.matches(".qcard")) {
         var id = clicked.dataset.id;
         var correct = checkAnswer(currentQuestion, id);
-        console.log(id);
 
         if (correct) {
             questionResultEl.textContent = "Correct!";
@@ -37,8 +53,9 @@ quizEl.addEventListener("click", function(event) {
     }
 });
 
-// function declarations
-// initializes basic set of four answers
+
+//******* function declarations *******//
+// initializes basic set of question and four answers
 function initQuestion(q) {
     // remove any lingering qCards
     var answersEl = document.querySelectorAll(".qcard");
@@ -63,6 +80,7 @@ function initQuestion(q) {
 
 // check if the provided answer is correct
 function checkAnswer(q, id) {
+    var correct = q.correct;
     // remove qcards from display
     questionEl.textContent = "";
     var answersEl = document.querySelectorAll(".qcard");
@@ -71,10 +89,14 @@ function checkAnswer(q, id) {
             answersEl[i].remove();
         }
     }
-    setTimeout(initQuestion, 1000, question1);
+    
+    // pose new random question after timeout
+    questions.splice(currentQuestionIndex, 1);
+    currentQuestionIndex = Math.floor(Math.random() * questions.length);
+    setTimeout(initQuestion, 1000, questions[currentQuestionIndex]);
 
     // now return bool (right or wrong)
-    if (parseInt(q.correct) === parseInt(id)) {
+    if (parseInt(correct) === parseInt(id)) {
         answersEl[parseInt(id)].setAttribute("style", "background-color: green");
         return true;
     }
