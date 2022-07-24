226bef1b1a85d96989362bf7343787de43910757
dan brackenbury <daniel.brackenbury@gmail.com>
2022-07-23 22:58:09 -0400

    css and html adjustments; core structure in place, game-end code in progress

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,10 +3,14 @@ var quizEl = document.querySelector("#quiz"); // query the quiz container and st
 var questionEl = document.querySelector("#qText"); // query the question header and store it (for updating text)
 var questionDescriptionEl = document.querySelector("#qDesc"); // query the question description and store it (also for updating text)
 var questionResultEl = document.querySelector("#qRes"); // query the feedback result (right or wrong) for updating
-
+var timerEl = document.querySelector("#timer"); // query the timer element
 // the question currently being displayed
 var currentQuestion = null;
 var currentQuestionIndex = 0;
+
+// tick timer
+var timer = 60; // default time for quiz is 60 seconds
+var timerIntervalId = null; // the id for the setInterval function
 
 // create questions object
 var questions = [
@@ -17,7 +21,7 @@ var questions = [
     },
     {
         question: "What is var f = function()?",
-        a: ["Function expression", "Function declaration", "Variable declaration", "Function equation"],
+        a: ["function expression", "function declaration", "variable declaration", "function equation"],
         correct: 0,
     },
     {
@@ -34,29 +38,78 @@ var questions = [
 
 
 //******** main body of logic code ********//
-currentQuestionIndex = Math.floor(Math.random() * questions.length);
-initQuestion(questions[currentQuestionIndex]);
+// set the initial count of the html element to the internal value
+timerEl.textContent = "Time: " + timer;
+
+// welcome the user on launch :^)
+welcomeScreen();
 
 // add listener to quiz container element to check its events for clicks
 quizEl.addEventListener("click", function(event) {
     var clicked = event.target;
     if (clicked.matches(".qcard")) {
-        var id = clicked.dataset.id;
-        var correct = checkAnswer(currentQuestion, id);
-
-        if (correct) {
-            questionResultEl.textContent = "Correct!";
+        if (clicked.matches("#start-button")) { // start game
+            // pick an initial random question
+            currentQuestionIndex = Math.floor(Math.random() * questions.length);
+            initQuestion(questions[currentQuestionIndex]);
+            // init timer
+            timerIntervalId = setInterval(decrementTimer, 1000);
+            timerEl.setAttribute("style", "color: green");
         }
-        else {
-            questionResultEl.textContent = "Wrong!";
+        else { // otherwise parse answer
+            var id = clicked.dataset.id;
+            var correct = checkAnswer(currentQuestion, id);
+
+            if (correct) {
+                questionResultEl.textContent = "Correct!";
+            }
+            else {
+                questionResultEl.textContent = "Wrong!";
+            }
         }
     }
 });
 
 
 //******* function declarations *******//
+// prep introductory screen
+function welcomeScreen() {
+    // clear placeholder text
+    questionResultEl.textContent = ""
+    // ensure timer is the right colour (just in case)
+    timerEl.setAttribute("style", "color: black");
+
+    // write intro to question element
+    questionEl.textContent = "Javascript Code Quiz Challenge"
+    // write description into its sub-element
+    questionDescriptionEl.textContent =
+    "Answer as many as questions as \
+    you can before time's up! Just be careful,\
+    because wrong answers steal time away from you!";
+
+    // prep start came button as qcard
+    var startButtonEl = document.createElement("div");
+    startButtonEl.className = "qcard";
+    startButtonEl.id = "start-button"; // set custom element id to be identified on-click
+    startButtonEl.textContent = "Start Quiz";
+    //startButtonEl.setAttribute("style", "text-align: center"); // center the button text
+    quizEl.appendChild(startButtonEl);
+}
+
+function gameOver() { // both for good jobs, *and* bad jobs!
+    if (questions.length === 0) { // all questions answered; good job!
+        
+    }
+    else { // ran out of time; bad job!
+
+    }
+}
+
 // initializes basic set of question and four answers
 function initQuestion(q) {
+    // remove the question result
+    questionResultEl.textContent = ""
+
     // remove any lingering qCards
     var answersEl = document.querySelectorAll(".qcard");
     for (var i = 0; i < answersEl.length; i++) {
@@ -93,15 +146,62 @@ function checkAnswer(q, id) {
     // pose new random question after timeout
     questions.splice(currentQuestionIndex, 1);
     currentQuestionIndex = Math.floor(Math.random() * questions.length);
-    setTimeout(initQuestion, 1000, questions[currentQuestionIndex]);
+    if (questions.length > 0) {
+        setTimeout(initQuestion, 1000, questions[currentQuestionIndex]);
+    }
+    else {
+        // end the game if no questions left
+        gameOver();
+    }
 
     // now return bool (right or wrong)
     if (parseInt(correct) === parseInt(id)) {
-        answersEl[parseInt(id)].setAttribute("style", "background-color: green");
+        answersEl[parseInt(id)].setAttribute("style", "background-color: lightgreen; border-color: green");
         return true;
     }
     else {
-        answersEl[parseInt(id)].setAttribute("style", "background-color: red");
+        answersEl[parseInt(id)].setAttribute("style", "background-color: lightcoral; border-color: purple");
         return false;
+    }
+}
+
+// in-game timer update
+function decrementTimer() {
+    // decrement and update timer
+    if (timer > 0)
+        timer--;
+    timerEl.textContent = "Time: " + timer
+
+    // change colour as early warning
+    if (timer < 20) { // 20 seconds left better be speedy...
+        timerEl.setAttribute("style", "color: red");
+    }
+
+    // is it at zero? break if so!
+    if (timer < 1) {
+        timer = 0; // ensure no negative values are being displayed
+        // stop the timer
+        clearInterval(timerIntervalId);
+
+        // reset timer colour
+        timerEl.setAttribute("style", "color: black");
+        gameOver();
+    }
+}
+
+// can be called arbitrarily to modify timer by set amount
+function updateTimer(amount) {
+    timer -= amount; // subtract given amount
+
+    if (timer < 1) {
+        timer = 0; // ensure no negative values are displayed
+        // stop timer
+        if (timerIntervalId != null) { // perform null check since this can be called arbitrarily
+            clearInterval(timerIntervalId)
+        }
+
+        // reset timer colour
+        timerEl.setAttribute("style", "color: black");
+        gameOver();
     }
 }
